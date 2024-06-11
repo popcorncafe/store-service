24840cce6f1e5f178eb20fc2457628beef199b97
Artem Nefedov <artemnefedov@duck.com>
2024-06-11 12:30:30 +0400

    created test classes for repositories

---
+++ src/test/java/com/popcorncafe/storeservice/repository/CartRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.popcorncafe.storeservice.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CartRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getByClient() {
+    }
+
+    @Test
+    void getByStore() {
+    }
+}
---
+++ src/test/java/com/popcorncafe/storeservice/repository/StoreRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.popcorncafe.storeservice.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StoreRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getByLocation() {
+    }
+}
