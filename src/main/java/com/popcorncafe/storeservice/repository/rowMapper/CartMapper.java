7daa53fd5f8aee13ab15662e2a953ba1bc98f691
Artem Nefedov <artemnefedov@duck.com>
2024-07-25 11:57:21 +0400

    fixed RowMapper work when working with Timestamp and Instant
    Signed-off-by: Artem Nefedov <artemnefedov@duck.com>

--- src/main/java/com/popcorncafe/storeservice/repository/rowMapper/CartMapper.java
+++ src/main/java/com/popcorncafe/storeservice/repository/rowMapper/CartMapper.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.Instant;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -13,9 +13,13 @@ public class CartMapper implements RowMapper<Cart> {
 
     @Override
     public Cart mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new Cart(rs.getObject("cart_id", UUID.class), rs.getLong("client_id"),
-                rs.getObject("store_id", UUID.class), rs.getObject("order_date", Instant.class),
-                rs.getLong("order_price"), Cart.Status.valueOf(rs.getString("status")), new ArrayList<>()
+        return new Cart(
+                rs.getObject("cart_id", UUID.class),
+                rs.getLong("client_id"),
+                rs.getObject("store_id", UUID.class),
+                rs.getObject("order_date", Timestamp.class).toInstant(),
+                rs.getLong("order_price"),
+                Cart.Status.valueOf(rs.getString("status")), new ArrayList<>()
         );
     }
 }
